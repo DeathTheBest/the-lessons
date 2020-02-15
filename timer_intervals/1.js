4ed8567ba29cfaa937fac14975715478d4c5f28c
DeathTheBest <goosesuka@gmail.com>
2020-02-15 15:17:18 +0200

    timer_interval
    timer_interval

---
+++ timer_intervals/1.js
@@ -0,0 +1,36 @@
+var sdvig = 0;
+var timer;
+
+var test = document.getElementById("test");
+
+//set interval (function_namme, time)
+// move();
+
+function move () {
+    test.style.marginLeft = sdvig +'px';
+    sdvig= sdvig +10;
+    timer = setTimeout(move, 50);
+    
+}
+// var timer = setInterval(move, 50);
+
+document.getElementById('stop').onclick = function () {
+    // clearInterval(timer);
+    clearTimeout(timer);
+}
+
+// setTimeout(move, 5000);
+
+var a =10;
+obr();
+function obr(){
+    document.getElementById('out').innerHTML =a;
+    a--;
+    if (a<0) {
+        clearTimeout(timer);
+        alert('таймер отключен');
+    }
+    else {
+        timer = setTimeout(obr,1000);
+    }
+}
